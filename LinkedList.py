3756d55bfb2cd078835815591eee4ede39d02835
Karan Deep Bajaj <karandeep.bajaj@blueyonder.com>
2025-06-14 16:16:54 +0530

    LL more

--- LinkedList.py
+++ LinkedList.py
@@ -5,4 +5,23 @@ class Node:
 class LinkedList:
     def __init__(self):
         self.head= None
+    def insert_at_begining(self,data):
+        node=Node(data,self.head)
+        self.head=node
 
+    def print(self):
+        if self.head is None:
+            print("Linked List Is empty")
+            return
+        itr=self.head
+        llstr=""
+        while itr:
+            llstr +=str(itr.data)+'-->'
+            itr=itr.next
+        print(llstr)
+
+if __name__ == '__main__':
+    ll=LinkedList()
+    ll.insert_at_begining(7)
+    ll.insert_at_begining(89)
+    ll.print()
