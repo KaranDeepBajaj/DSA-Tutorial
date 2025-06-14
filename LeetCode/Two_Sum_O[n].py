1d951e5e77be89cf70b91b74dd813232d79211da
Karan Deep Bajaj <karandeep.bajaj@blueyonder.com>
2025-06-14 23:48:27 +0530

    Leet Code -1

--- LeetCode/Two_Sum_O[n].py
+++ LeetCode/Two_Sum_O[n].py
@@ -1,18 +1,16 @@
 class Solution:
-    def twoSum(self,nums,target):
-        l=len(nums)
-        list=[]
-        for i in range(0,l):
-            for k in range (i+1,l):
-                if (i<l+1):
-                    sum= nums[i]+nums[k]
-                    if(sum==target):
-                        list.append(i)
-                        list.append(k)
-                        return list
+    def twoSum(self, nums, target):
+        seen = {}
+        for i, num in enumerate(nums):
+            complement = target - num
+            print(complement)
+            if complement in seen:
+                return [seen[complement], i]
+            seen[num] = i
+            print(seen)
 
 if __name__ == '__main__':
     obj=Solution()
-    list=obj.twoSum([3,3],6)
-    # list=obj.twoSum([-1,-2,-3,-4,-5],-8)
+    # list=obj.twoSum([3,3],6)
+    list=obj.twoSum([-1,-2,-3,-4,-5],-8)
     print(list)
