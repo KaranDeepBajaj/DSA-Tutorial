d87a4501af488133b724a93aa33d407700280775
Karan Deep Bajaj <karandeep.bajaj@blueyonder.com>
2025-06-16 12:24:12 +0530

    Leet Code 3rd Question Solution

--- LeetCode/3_Longest_Substring_Without_Repeating_Characters_O[n].py
+++ LeetCode/3_Longest_Substring_Without_Repeating_Characters_O[n].py
@@ -2,15 +2,16 @@ class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         char_set=set()
         left =0
+        max_length=0
         for right in range(len(s)):
             while s[right] in char_set:
-                char_set.remove(s[left])
+                char_set.remove(s[left]) # this will remove element form char_set unitl duplicate value is removed
                 left+=1
             char_set.add(s[right])
-            max_length=max(right-left+1,max_length)
+            max_length=max(len(char_set),max_length)
         return max_length
 
 if __name__ == '__main__':
     obj=Solution()
-    m=obj.lengthOfLongestSubstring("")
+    m=obj.lengthOfLongestSubstring("abcdabcde")
     print(m)
