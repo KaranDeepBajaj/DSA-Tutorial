d5bf18c5925028e9dbecc0f0bcd1a56f33c73e1c
Karan Deep Bajaj <karandeep.bajaj@blueyonder.com>
2025-06-17 16:02:18 +0530

    Leet Code 5th Question Solution

---
+++ LeetCode/5_Longest_Palindromic_Substring.py
@@ -0,0 +1,25 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        lt=0
+        if (len(s)==1):
+            final=s
+            return (final)
+        for i in range(len(s)):
+            l=s[i]
+            for k in range(i+1,len(s)):
+                l=l+s[k]
+                l=l
+                if l==l[::-1]:
+                    if(len(l)>lt):
+                        lt=len(l)
+                        final=l
+        if (lt==0):
+            final=s[0]
+            return (final)
+        else:
+            return(final)
+
+if __name__ == '__main__':
+    obj=Solution()
+    o= obj.longestPalindrome("ac")
+    print(o)
