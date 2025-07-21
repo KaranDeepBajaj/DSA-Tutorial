5baa68c57b233a178738cefcb3f2f0f7f1134218
Karan Deep Bajaj <karandeep.bajaj@blueyonder.com>
2025-07-21 14:42:16 +0530

    20th Leet Code

---
+++ HackerRank/Arrays.py
@@ -0,0 +1,11 @@
+#!/bin/python3
+
+import os
+
+def reverseArray(a):
+    res = a[::-1]
+    return res
+
+
+if __name__ == '__main__':
+    print(reverseArray([1,2,3,4]))
--- LeetCode/19_Remove_Nth_Node_From_End_of_List.py
+++ LeetCode/19_Remove_Nth_Node_From_End_of_List.py
@@ -4,13 +4,38 @@ class ListNode:
         self.val = val
         self.next = next
 class Solution:
-    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        print(head)
+
+    def linkedlist_to_list(head):
+        result = []
+        while head:
+            result.append(head.val)
+            head = head.next
+        return result
+    def removeNthFromEnd(head: ListNode, n: int) -> ListNode:
+        dummy = ListNode(0, head)
+        fast = slow = dummy
+
+        # Move fast n steps ahead
+        for _ in range(n):
+            fast = fast.next
+        print(fast)
+
+        # Move fast to the end, maintaining the gap
+        while fast.next:
+            fast = fast.next
+            slow = slow.next
+
+        # Skip the desired node
+        slow.next = slow.next.next
+
+        return dummy.next
+
 
 
 
 if __name__ in '__main__':
-    obj=Solution()
-    l=ListNode()
-    o=obj.removeNthFromEnd([1,2,3,4,5],2)
-    print(o)
+    obj = Solution()
+    head = obj.list_to_linkedlist([1, 2, 3, 4, 5])
+    n = 2
+    result = obj.removeNthFromEnd(head, n)
+    print(obj.linkedlist_to_list(result))
