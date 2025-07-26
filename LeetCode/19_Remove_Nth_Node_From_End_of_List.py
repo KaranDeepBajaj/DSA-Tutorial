0ba8ae5f019fde549ee78bda18cfb44fedd65ac9
Karan Deep Bajaj <karandeep.bajaj@blueyonder.com>
2025-07-26 18:53:57 +0530

    LeetCode-19

--- LeetCode/19_Remove_Nth_Node_From_End_of_List.py
+++ LeetCode/19_Remove_Nth_Node_From_End_of_List.py
@@ -1,41 +1,29 @@
 # Definition for singly-linked list.
+from typing import Optional
+
+
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
 class Solution:
-
-    def linkedlist_to_list(head):
-        result = []
-        while head:
-            result.append(head.val)
-            head = head.next
-        return result
-    def removeNthFromEnd(head: ListNode, n: int) -> ListNode:
-        dummy = ListNode(0, head)
-        fast = slow = dummy
-
-        # Move fast n steps ahead
-        for _ in range(n):
-            fast = fast.next
-        print(fast)
-
-        # Move fast to the end, maintaining the gap
-        while fast.next:
-            fast = fast.next
-            slow = slow.next
-
-        # Skip the desired node
-        slow.next = slow.next.next
-
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        dummy=ListNode(0)
+        dummy.next=head
+        first=second=dummy
+        for i in range(n+1):
+            first=first.next
+        while first:
+            second=second.next
+            first=first.next
+        second.next=second.next.next
         return dummy.next
-
 
 
 
 if __name__ in '__main__':
     obj = Solution()
-    head = obj.list_to_linkedlist([1, 2, 3, 4, 5])
+    head = [1, 2, 3, 4, 5]
     n = 2
     result = obj.removeNthFromEnd(head, n)
     print(obj.linkedlist_to_list(result))
