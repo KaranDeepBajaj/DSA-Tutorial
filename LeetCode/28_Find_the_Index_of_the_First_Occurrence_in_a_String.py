568073158b61e14c812d75e10ca468d7312bfa87
Karan Deep Bajaj <karandeep.bajaj@blueyonder.com>
2025-07-30 14:22:36 +0530

    LeetCode-28

--- LeetCode/28_Find_the_Index_of_the_First_Occurrence_in_a_String.py
+++ LeetCode/28_Find_the_Index_of_the_First_Occurrence_in_a_String.py
@@ -1,8 +1,17 @@
-import unittest
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        l=len(needle)
+        l1=len(haystack)
+        for i in range(l1):
+            result=haystack[i:i+l]
+            if result==needle:
+                return i
+        if i+1==l1:
+            return -1
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+
 
 if __name__ == '__main__':
-    unittest.main()
+    obj=Solution()
+    o=obj.strStr("hello","ll")
+    print(o)
